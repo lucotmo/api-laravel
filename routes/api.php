e98dd80bd0a6650d00c6ba154d8adaf7d8d93460
lucotmo <lucotmo@gmail.com>
2019-03-17 18:38:14 -0500

    implementando las operaciones complejas para category

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,10 @@ Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' =>
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => ['index']]);
+Route::resource('categories.products', 'Category\CategoryProductController', ['only' => ['index']]);
+Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only' => ['index']]);
+Route::resource('categories.transactions', 'Category\CategoryTransactionController', ['only' => ['index']]);
 
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 
