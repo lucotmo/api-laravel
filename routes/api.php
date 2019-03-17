0ef57b35021ca411d322d6c1599a18ffb9b634cf
lucotmo <lucotmo@gmail.com>
2019-03-17 17:59:08 -0500

    implementando las operaciones complejas con buyer

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ use Illuminate\Http\Request;
 });*/
 
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
 
