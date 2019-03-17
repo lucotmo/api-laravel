3e49f14f6b43589d4683e5e71d3d43d37e3ee414
lucotmo <lucotmo@gmail.com>
2019-03-17 14:56:31 -0500

    inyección implicita de modelos en rutas y metodos

--- app/Buyer.php
+++ app/Buyer.php
@@ -3,10 +3,17 @@
 namespace App;
 
 use App\Transaction;
+use App\Scopes\BuyerScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Buyer extends User
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new BuyerScope);
+    }
     //
     public function transactions()
     {
