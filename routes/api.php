ab36a8c4e01e6eb881a7a948506b2a55f581b1b6
lucotmo <lucotmo@gmail.com>
2019-03-17 16:49:59 -0500

    implementando la operacines complejas con transaction

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,8 @@ Route::resource('categories', 'Category\CategoryController', ['except' => ['crea
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
 
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 
